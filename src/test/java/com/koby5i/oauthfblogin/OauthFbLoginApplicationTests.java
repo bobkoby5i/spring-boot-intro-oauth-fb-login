9e967eacb84b4c19f4448e5b0705dee84bcfff0d
koby5i <bobkoby5i@xdeq.com>
2020-01-10 00:04:42 +0100

    initial commit

---
+++ src/test/java/com/koby5i/oauthfblogin/OauthFbLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koby5i.oauthfblogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthFbLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
